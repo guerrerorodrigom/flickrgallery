0f16a1c8b25f61ffb61aa220b3430e0cad1f57e0
Rodrigo Guerrero <rodrigo.guerreromulia@jumbo.com>
2022-12-10 16:31:53 +0100

    Clear search query.

--- app/src/main/java/com/rodrigoguerrero/flickrgallery/presentation/components/SearchTextField.kt
+++ app/src/main/java/com/rodrigoguerrero/flickrgallery/presentation/components/SearchTextField.kt
@@ -1,5 +1,6 @@
 package com.rodrigoguerrero.flickrgallery.presentation.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,12 +12,9 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -52,6 +50,15 @@ fun SearchTextField(
                     text = stringResource(id = R.string.search_hint),
                     style = MaterialTheme.typography.body1
                 )
+            },
+            trailingIcon = {
+                if (searchQuery.isNotEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = null,
+                        modifier = Modifier.clickable { onValueChanged("") }
+                    )
+                }
             }
         )
     }
